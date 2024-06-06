2086b79b9506e90bbb5cbb11d6153782c17eb2e2
eolybq <jez.vasek@gmail.com>
2024-06-06 14:31:43 +0200

    first settings

---
+++ init.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.opt.showmode = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.o.cursorline = true
+vim.o.termguicolors = true
+
+require("vim-options")
+require("lazy").setup("plugins")
